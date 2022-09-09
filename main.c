400400b644420b2c5066aa5aaba47a140323113a
Lyndonn Pamintuan <pamintuan.lyndonn0503@gmail.com>
2022-09-09 11:45:03 +0800

    Add gameplays to choose by the player

--- main.c
+++ main.c
@@ -9,10 +9,11 @@ typedef struct Player
 {
     char rep;
     int turn;
+    bool is_ai;
     void (*move)(char (*board)[UNITS], short *point, struct Player *player);
 } Player;
 
-void enter_player(Player players[PLAYER_NO], bool is_ai, short turn);
+void enter_player(Player players[PLAYER_NO], bool is_ai, short turn, char rep);
 void print_board(char (*board)[UNITS]);
 void player_move(char (*board)[UNITS], short *point, Player *player);
 void ai_move(char (*board)[UNITS], short *point, Player *player);
@@ -24,35 +25,52 @@ bool validate_diag();
 
 int main()
 {
-
+    int gameplay = -1;
     char board[][UNITS] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    char reps[] = {'x', 'o'};
     Player players[PLAYER_NO];
-    short curr_turn = 0, curr_player = -1;
-    short curr_point[] = {0, 0};
+    short curr_turn = 0, curr_player = -1, curr_point[] = {0, 0};
+
+    puts("Choose opponent ( 1 => AI - Player, 2 => Player - AI, (any int) => Player - Player ):");
+    scanf(" %d", &gameplay);
+    if(gameplay != 1 || gameplay != 2)
+        gameplay = -1;
 
     for (int x = 0; x < PLAYER_NO; x++)
-        enter_player(players, ((x+1) % 2) == 0, x); // ai the 2nd player automatically for now
-    print_board(board);
+    {
+        switch (gameplay)
+        {
+        case 1:
+            enter_player(players, ((x + 2) % 2) == 0, x, reps[(x + 2) % 2]);
+            break;
+        case 2:
+            enter_player(players, ((x + 1) % 2) == 0, x, reps[x]);
+            break;
+        default:
+            enter_player(players, false, x, reps[x]);
+            break;
+        }
+    }
 
+    print_board(board);
     while (true)
     {
         curr_player = (++curr_player) % PLAYER_NO;
         players[curr_player].move(board, curr_point, &players[curr_player]);
         curr_turn++;
-        if ((curr_player % PLAYER_NO) != 0)
+        if (players[curr_player].is_ai || gameplay == -1)
             print_board(board);
-        if(curr_turn >= (UNITS*UNITS))
+        if (curr_turn >= (UNITS * UNITS))
             break;
     }
     print_board(board);
     return 0;
 }
 
-void enter_player(Player player[2], bool is_ai, short turn)
+void enter_player(Player player[2], bool is_ai, short turn, char rep)
 {
-    char rep = (is_ai) ? 'o' : 'x';
     void *move = (is_ai) ? ai_move : player_move;
-    Player new_player = {.rep = rep, .turn = turn, .move = move};
+    Player new_player = {.rep = rep, .turn = turn, .move = move, .is_ai = is_ai};
     player[turn] = new_player;
 }
 
@@ -62,12 +80,11 @@ void player_move(char (*board)[UNITS], short *point, Player *player)
     scanf("%hi", &point[0]);
     puts("Enter the column (1 - 3):");
     scanf(" %hi", &point[1]);
-    board[point[0] - 1][point[1] - 1] = 'x';
+    board[point[0] - 1][point[1] - 1] = player->rep;
 }
 
 void ai_move(char (*board)[UNITS], short *point, Player *player)
 {
-    char rep = 'o';
     srand(time(0));
     printf("AI thinking...");
     point[0] = rand() % UNITS;
@@ -78,7 +95,8 @@ void ai_move(char (*board)[UNITS], short *point, Player *player)
         point[0] = rand() % UNITS;
         point[1] = rand() % UNITS;
     }
-    board[point[0]][point[1]] = rep;
+    board[point[0]][point[1]] = player->rep;
+    puts("");
 }
 
 void print_board(char (*board)[UNITS])
@@ -90,10 +108,7 @@ void print_board(char (*board)[UNITS])
     {
         char rep_col[UNITS];
         for (int j = 0; j < UNITS; j++)
-        {
-            char in = board[x][j];
-            rep_col[j] = in;
-        }
+            rep_col[j] = board[x][j];
         puts("|     |     |     |");
         printf("|  %c  |  %c  |  %c  |\n", rep_col[0], rep_col[1], rep_col[2]);
         puts("|_____|_____|_____|");
